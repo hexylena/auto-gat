3a91c0488c6b6b9462ea8ecc9d13bf1302f47118
Helena Rasche <hxr@hx42.org>
2021-03-23 20:21:52 +0100

    sleepy

--- demo.sh
+++ demo.sh
@@ -11,15 +11,17 @@ clear
 # Install ansible (move to pre-setup?)
 cd git-gat
 # Show what's there
-pe "ls -al"
 
 # We'll loop over the commits in the repo, one by one
-for commit in $(git log --pretty=oneline | tac | head -n 10 | cut -f 1 -d' '); do
-	git checkout $commit
-	echo "Next step: $(git show | head -n 5 |tail -n 1 | sed 's/[^:]*: //g')"
-	# echo "File changed: $(git show --name-only | tail -n 1)"
+for commit in $(git log --pretty=oneline | tac | head -n 10 | tail -n 9 | cut -f 1 -d' '); do
+	git checkout -q $commit
+	echo "$(tput bold) Next step: $(git show | head -n 5 |tail -n 1 | sed 's/[^:]*: //g') $(tput sgr0)"
+	sleep 2
+	echo "File changed: $(git show --name-only | tail -n 1)"
+	sleep 2
 	# Show the commit
 	git show --pretty | tail -n+9
+	sleep 5
 
 	# If the requirements file was changed, then we need to re-run ansible-galaxy install
 	reqs=$(git show --name-only | grep --quiet requirements.yml)
@@ -28,9 +30,11 @@ for commit in $(git log --pretty=oneline | tac | head -n 10 | cut -f 1 -d' '); d
 		echo "The requirements have changed."
 		pe "ansible-galaxy install -p roles -r requirements.yml"
 	fi
+	sleep 3
 
 	if [[ -f galaxy.yml ]]; then
 		echo "Let's re-run the playbook"
 		pe "ansible-playbook galaxy.yml"
 	fi
+	sleep 3
 done
