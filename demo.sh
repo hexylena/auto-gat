c860798d926936209e2f6618502d927030345aff
Helena Rasche <hxr@hx42.org>
2021-03-23 20:00:24 +0100

    ok? with ansible?

--- demo.sh
+++ demo.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 . ./demo-magic/demo-magic.sh -n
 clear
-pe "pwd"
+pe "pip install ansible"
+pe "git clone https://github.com/hexylena/git-gat/"
+pe "cd git-gat"
 pe 'git status'
-pe 'git log --oneline --decorate -n 20 | cat'
-pe "ls"
+for commit in $(git log --pretty=oneline | tac | head -n 4 | cut -f 1 -d' '); do
+	pe "git checkout $commit"
+	echo "Here's what changed"
+	pe "git show"
+	pe "ls -al"
+	reqs=$(git show --name-only | grep --quiet requirements.yml)
+	ec=$?
+	if (( ec == 0 )); then
+		echo "The requirements have changed."
+		pe "ansible-galaxy install -p roles -r requirements.yml"
+	fi
+done
