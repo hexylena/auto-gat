3a942b82557a6b0ac474dd2d5a4f8dc95199031f
Helena Rasche <hxr@hx42.org>
2021-11-05 13:16:14 +0100

    it liiiiives

--- demo.sh
+++ demo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+OUR_DIR=$(pwd)
 # Disable git's paging which can cause a hang
 export GIT_PAGER=cat
 export GALAXY_HOSTNAME="$(hostname -f)"
@@ -12,39 +13,46 @@ echo "$(hostname -f) ansible_connection=local ansible_user=$(whoami)"  >> ~/.hos
 # Prevent pip from shouting everywhere
 pip config --user set global.progress_bar off
 # Setup the demo-magic
-. ./demo-magic/demo-magic.sh -n
+. ${OUR_DIR}/demo-magic/demo-magic.sh -n
 # Hide our demo-magic activation
 clear
 
 # Install ansible (move to pre-setup?)
 cd git-gat
+pwd
+ls
+
 # Show what's there
 ansible-galaxy install -p roles galaxyproject.self_signed_certs > /dev/null 2>/dev/null
 
 # We'll loop over the commits in the repo, one by one
-for commit in $(git log --pretty=oneline | tac | head -n 10 | tail -n 9 | cut -f 1 -d' '); do
-	git checkout -q $commit
-	echo "$(tput bold)Next step: $(git show | head -n 5 |tail -n 1 | sed 's/[^:]*: //g') $(tput sgr0)"
-	sleep 2
-	echo "File changed: $(git show --name-only | tail -n 1)"
-	sleep 2
-	# Show the commit
-	git show --pretty | tail -n+9
-	sleep 5
+OLDIFS="$IFS"
+IFS=$'\n'
+for thing in $(cat .scripts/10-ansible-galaxy-script.txt); do
+	if [[ "$thing" == "git checkout"* ]]; then
+		# Checkout this commit
+		bash -c "$thing"
+		current_Commit=$(git show --format=%H | head -n 1)
 
-	# If the requirements file was changed, then we need to re-run ansible-galaxy install
-	reqs=$(git show --name-only | grep --quiet requirements.yml)
-	ec=$?
-	if (( ec == 0 )); then
+		echo "$(tput bold)Next step: $(git show --format="%s" | head -n 1 | sed 's/[^:]*: //g') $(tput sgr0)"
+		sleep 2
+
+		# Checkout the previous commit so we can show the diff properly
+		git checkout -q "$current_Commit^1"
+		# This will pretend to edit it in """"vim""""
+		python3 ${OUR_DIR}/diffplayer.py \
+			--diff <(git show "$current_Commit") \
+			--nosave \
+			--speed 0.1
+		# And revert back to the commit we said we're on
+		git checkout -q "$current_Commit"
+	elif [[ "$thing" ==  "ansible-galaxy"* ]]; then
 		echo "The requirements have changed, we'll need to install the new ones."
 		pe "ansible-galaxy install -p roles -r requirements.yml"
-	fi
-	sleep 3
 
-	if [[ -f galaxy.yml ]]; then
+	elif [[ "$thing" ==  "ansible-playbook"* ]]; then
 		echo "Let's re-run the playbook"
 		p "ansible-playbook galaxy.yml -u ${USER}"
 		ansible-playbook galaxy.yml -u ${USER} -e "nginx_ssl_role=galaxyproject.self_signed_certs openssl_domains={{ certbot_domains }}" --vault-password-file ~/.vault-password.txt -i ~/.hosts
 	fi
-	sleep 3
 done
