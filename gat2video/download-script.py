9697409dbec16288f74321d5a8b5c0006db49edb
Helena Rasche <hxr@hx42.org>
2022-04-14 15:52:51 +0200

    support loading directly

--- gat2video/download-script.py
+++ gat2video/download-script.py
@@ -13,6 +13,7 @@ GIT_GAT = os.path.expanduser('~/galaxy')
 parser = argparse.ArgumentParser(description='Download and generate a script')
 parser.add_argument('tutorial')
 parser.add_argument('--branch', default='main')
+parser.add_argument('--markdown', type=argparse.FileType('r'), help="Load from a markdown file rather than a URL")
 args = parser.parse_args()
 
 if args.tutorial != 'cvmfs':
@@ -20,9 +21,13 @@ if args.tutorial != 'cvmfs':
     sys.exit(1)
 
 # url = f'https://cdn.jsdelivr.net/gh/galaxyproject/training-material@{args.branch}/topics/admin/tutorials/{args.tutorial}/tutorial.md'
-url = f'https://raw.githubusercontent.com/galaxyproject/training-material/{args.branch}/topics/admin/tutorials/{args.tutorial}/tutorial.md'
-print(url)
-response = urllib.request.urlopen(url).read().decode('utf-8').split('\n')
+if args.markdown:
+    response = args.markdown.read().split('\n')
+else:
+    url = f'https://raw.githubusercontent.com/galaxyproject/training-material/{args.branch}/topics/admin/tutorials/{args.tutorial}/tutorial.md'
+    print(url)
+    response = urllib.request.urlopen(url).read().decode('utf-8').split('\n')
+
 lines = [x + '\n' for x in response]
 
 script = video_extract_script.process(lines)
